e4596e0c5dff13f1da22bbb789fbf6c90d0fe1ff
WataruNishimura <wataru.n@chot-inc.com>
2026-02-10 14:32:41 +0900

    fix: nvim

--- private_dot_config/nvim/lua/keymaps.lua
+++ private_dot_config/nvim/lua/keymaps.lua
@@ -1,3 +1,5 @@
+vim.g.mapleader = ' '
+
 local opts = { noremap = true, silent = true }
 local term_opts = { silent = true }
 
@@ -6,4 +8,12 @@ local keymap = vim.api.nvim_set_keymap
 
 --Map Ctrl-C to Esc
 keymap("n", "<C-c>", "Esc", opts)
+
+vim.keymap.set('n', '<leader>e', '<Cmd>Neotree toggle<CR>')
+
+-- Escでターミナルモードから抜ける
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
+
+-- ショートカットでターミナルを開く
+vim.keymap.set('n', '<leader>t', ':split | terminal<CR>')
 
