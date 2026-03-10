e41a00ce4d52eec437cd8efd70c5d77e174ea0a3
WataruNishimura <wataru.n@chot-inc.com>
2026-03-10 20:20:15 +0900

    fix typo

--- private_dot_config/nvim/lua/qall.lua
+++ private_dot_config/nvim/lua/qall.lua
@@ -5,7 +5,7 @@ nvim.api.nvim_create_user_command(
     for _, win in ipairs(vim.api.nvim_list_wins()) do
       if win ~= current_win then
         local buf = vim.api.nvim_win_get_buf(win)
-        if vim.do[buf].buftype == '' then
+        if vim.bo[buf].buftype == '' then
           return
         end
       end
